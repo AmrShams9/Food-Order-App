763e8d80e3d8adaff2510914d003e25f7630956e
Amr Shams <amrkhaled104@gmail.com>
2024-07-25 11:26:40 +0300

    Adding new Components

--- Food_Ordering_website/Website/src/App.jsx
+++ Food_Ordering_website/Website/src/App.jsx
@@ -1,12 +1,18 @@
-import Header from "./Components/Header";
-import Meals from "./Components/Meals";
+import Cart from './Components/Cart.jsx';
+import Header from './components/Header.jsx';
+import Meals from './components/Meals.jsx';
+import { CartContextProvider } from './Store/CartContext.jsx';
+import { UserProgressContextProvider } from './Store/UseProgressContext.jsx';
 
 function App() {
   return (
-    <>
-      <Header />
-      <Meals />
-    </>
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header />
+        <Meals />
+        <Cart />
+      </CartContextProvider>
+    </UserProgressContextProvider>
   );
 }
 
